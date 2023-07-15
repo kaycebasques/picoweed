7be6821c915b74fa92a96ab4183e546137a2fbe2
Kayce Basques <kayce@google.com>
2023-07-14 18:13:44 -0700

    feat: Get Pico SDK usage compiling again

--- src/main.cc
+++ src/main.cc
@@ -12,13 +12,21 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+#include "pico/stdlib.h"
 #include "pw_log/log.h"
 
 namespace pw::system {
 
 void UserAppInit() {
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         PW_LOG_INFO("Hello, Pigweed!");
+	gpio_put(LED_PIN, 1);
+	sleep_ms(250);
+	gpio_put(LED_PIN, 0);
+	sleep_ms(250);
     }
 }
 
